71c602acd7a3d1fbdb7b4a8fe278a7c98ead950e
Dns <102502216+DNS239@users.noreply.github.com>
2023-08-05 16:09:16 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você utrapasso a velocida permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
