dae6f40d0cb9a4d43b8af4e71fe39598a68d11c4
Dns <102502216+DNS239@users.noreply.github.com>
2023-08-07 22:47:18 -0300

    ex013-ex016

---
+++ aula013/ambiente.js
@@ -0,0 +1,19 @@
+for (var c = 1; c<=100; c++) {
+    console.log(`Passo ${c}`)
+}
+
+
+
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 10) /
+/
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
