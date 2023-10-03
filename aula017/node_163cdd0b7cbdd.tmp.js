92de5d2bc206cf69b32c76ebe93963877d5400ec
Dns <102502216+DNS239@users.noreply.github.com>
2023-10-02 23:30:29 -0300

    Delete node_163cdd0b7cbdd.tmp.js

--- aula017/node_163cdd0b7cbdd.tmp.js
+++
@@ -1,9 +0,0 @@
-let amigo = {nome: 'Dns', 
-sexo: 'M', 
-peso: 58.9,
-engordar(p= 0){
-    console.log('Engordou')
-    this.peso += p
-} }
-amigo.engordar(5)
-console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
